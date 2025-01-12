4261659ec15eb1c730262bd3a1d79d5435c640dd
bozkaISSO <zewazla@gmail.com>
2025-01-12 20:33:49 +0100

    feat: Added conditional message rendering

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import './App.css';
-import {useState} from "react";
+import React, { useState } from 'react';
 
 function App() {
-const [title, setTitle] = useState('Wall-E');
+    const [title, setTitle] = useState('Wall-E');
 
-function handleChange(event) {
+    function handleChange(event) {
         setTitle(event.target.value);
+    }
+
+    let message;
+    if (title.length < 5) {
+        message = "Tytuł jest za krótki. Nagrywają takie filmy?";
+    } else if (title.length < 15) {
+        message = "Tytuł jest ekstra, w sam raz na plakat przed kinem!";
+    } else {
+        message = "Tytuł jest za długi, nikt tego nie zapamięta.";
     }
 
     return (
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
-            <input type="text" value={title} onChange={handleChange}/>
+            <input type="text" value={title} onChange={handleChange} />
+            {title.length > 0 && <div>{message}</div>}
         </div>
     );
 }
 
 export default App;
+
 
