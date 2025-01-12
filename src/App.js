0f514184eef270f91d552ad68c69951a520de066
bozkaISSO <zewazla@gmail.com>
2025-01-12 20:18:00 +0100

    feat: Add input and handleChange in console

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 
 function App() {
+    let title = 'Wall-E';
+
+    function handleChange(event) {
+        console.log(event.target.value);
+    }
+
     return (
         <div>
             <h1>My favourite movies to watch</h1>
+            <h2>My favourite movie for today is {title}</h2>
+            <input type="text" onChange={handleChange} />
         </div>
     );
 }
 
 export default App;
+
