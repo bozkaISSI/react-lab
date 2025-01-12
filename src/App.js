6413900347c9459445e16a04f0671216fa705574
bozkaISSO <zewazla@gmail.com>
2025-01-12 20:45:57 +0100

    feat: Rendering list

--- src/App.js
+++ src/App.js
@@ -17,9 +17,17 @@ function App() {
         message = "Tytuł jest za długi, nikt tego nie zapamięta.";
     }
 
+    const movies = [
+    {title: "Wall-E"},
+    {title: "Pulp Fiction"},
+    {title: "Matrix"},
+    {title: "1670"},
+];
     return (
         <div>
             <h1>My favourite movies to watch</h1>
+            <h2>Titles</h2>
+            <ul>{movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}</ul>
             <h2>My favourite movie for today is {title}</h2>
             <input type="text" value={title} onChange={handleChange}/>
             <button type="button" onClick={()=>alert(title)}>Pokaż tytuł filmu</button>
