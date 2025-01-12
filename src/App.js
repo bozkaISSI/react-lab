9710c7f9916753657d5932f4bd82d83148a19ce1
bozkaISSO <zewazla@gmail.com>
2025-01-12 21:03:53 +0100

    feat: Mapping list

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import React, { useState } from 'react';
 
 function App() {
     const [title, setTitle] = useState('Wall-E');
+    const [movies, setMovies] = useState([]);
 
     function handleChange(event) {
         setTitle(event.target.value);
+    }
+
+    function handleAddMovie() {
+        if (title.trim()) {
+            const newMovie = { title };
+            setMovies([...movies, newMovie]);
+            setTitle('');
+        } else {
+            alert('Tytuł filmu nie może być pusty!');
+        }
     }
 
     let message;
@@ -18,25 +29,22 @@ function App() {
         message = "Tytuł jest za długi, nikt tego nie zapamięta.";
     }
 
-    const movies = [
-    {title: "Wall-E"},
-    {title: "Pulp Fiction"},
-    {title: "Matrix"},
-    {title: "1670"},
-];
     return (
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>Titles</h2>
-            <ul>{movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}</ul>
+            <ul>
+                {movies.map((movie, index) => (
+                    <li key={index}>{movie.title}</li>
+                ))}
+            </ul>
             <h2>My favourite movie for today is {title}</h2>
-            <input type="text" value={title} onChange={handleChange}/>
-            <button type="button" onClick={()=>alert(title)}>Pokaż tytuł filmu</button>
+            <input type="text" value={title} onChange={handleChange} />
+            <button type="button" onClick={handleAddMovie}>Add Movie</button>
+            <button type="button" onClick={() => alert(title)}>Pokaż tytuł filmu</button>
             {title.length > 0 && <div>{message}</div>}
         </div>
     );
 }
 
 export default App;
-
-
