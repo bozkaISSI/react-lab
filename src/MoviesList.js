d6a9e1a38ea2eaab30d57512c985278abb857e9e
bozkaISSO <zewazla@gmail.com>
2025-01-13 01:28:33 +0100

    feat: Added the ability to delete a video

--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,12 +1,33 @@
 import Movie from "./Movie";
+import { FaTrash } from "react-icons/fa";
 
-export default function MoviesList({ movies }) {
+export default function MoviesList({ movies, onRemoveMovie }) {
     return (
         <div>
             <h2>Movies List</h2>
             <ul>
                 {movies.map((movie, index) => (
-                    <Movie key={index} title={movie.title} year={movie.year} />
+                    <li
+                        key={index}
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                            padding: "5px 0",
+                        }}
+                    >
+                        <span style={{display: "flex", alignItems: "center"}}>
+                            <Movie title={movie.title} year={movie.year}/>
+                        </span>
+                        <FaTrash
+                            onClick={() => onRemoveMovie(index)}
+                            style={{
+                                color: "grey",
+                                cursor: "pointer",
+                                fontSize: "20px",
+                            }}
+                            title="Remove movie"
+                        />
+                    </li>
                 ))}
             </ul>
         </div>
