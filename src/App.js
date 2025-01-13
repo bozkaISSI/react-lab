10218f632bff6c4217467516ca4b7f70894ba6f7
bozkaISSO <zewazla@gmail.com>
2025-01-13 01:15:28 +0100

    feat: Displaying a form when a button is clicked

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React, { useState } from 'react';
-import MoviesList from './MoviesList';
-import MovieForm from './MovieForm';
+import React, { useState, useEffect } from "react";
+import MoviesList from "./MoviesList";
+import MovieForm from "./MovieForm";
 import "milligram";
 
 function App() {
     const [movies, setMovies] = useState([]);
+    const [showForm, setShowForm] = useState(false);
+
+    useEffect(() => {
+        document.title = "My Movie App";
+    }, []);
+
+    function addMovie(movie) {
+        setMovies([...movies, movie]);
+        setShowForm(false);
+    }
+
+    function removeMovie(index) {
+        setMovies(movies.filter((_, i) => i !== index));
+    }
 
     return (
         <div>
-            <h1>My favourite movies to watch</h1>
-            <MoviesList movies={movies} />
-            <MovieForm
-                onMovieSubmit={(movie) => setMovies([...movies, movie])}
-                buttonLabel="Add a movie"
-            />
+            <h1>My Favourite Movies to Watch</h1>
+
+            {movies.length === 0 ? (
+                <p>No movies added yet. Add your first movie!</p>
+            ) : (
+                <MoviesList movies={movies} onRemoveMovie={removeMovie} />
+            )}
+
+            {!showForm && (
+                <button onClick={() => setShowForm(true)}>Add a Movie</button>
+            )}
+
+            {showForm && (
+                <MovieForm
+                    onMovieSubmit={addMovie}
+                    buttonLabel="Add a movie"
+                />
+            )}
         </div>
     );
 }
